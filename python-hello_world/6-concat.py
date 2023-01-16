b2e76ed1551ba4cee744c722a86179e7fb357099
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-16 19:14:28 +0000

    Fix pycodestyle violation in file 6

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Holberton" # joins " " and 'str2' together in a new 'str' object,
-str2 = "School"    # joins that string with 'str1' (in the same order the code is written)
-str1 += " " + str2 # to make a 3rd 'str' object, and makes 'str1' point to it
+str1 = "Holberton"  # joins " " and 'str2' together in a new 'str'
+str2 = "School"     # object, joins 'str1' with that string to make
+str1 += " " + str2  # a 3rd 'str' object, and makes 'str1' point to it
 print(f"Welcome to {str1}!")
