d195098aabb8cb76007217f54d87da00133bfb0e
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-19 15:16:58 +0000

    Add .format method requirement

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        output_char = c
+        not_lowercase_output_char = c
 
         is_lowercase = ord(c) >= 97 and ord(c) <= 122
         if is_lowercase:
-            output_char = chr(ord(c) - 32)
-            # make uppercase
+            not_lowercase_output_char = chr(ord(c) - 32)
 
-        print(output_char, end="")
+        print("{}".format(not_lowercase_output_char), end="")
+    print()
