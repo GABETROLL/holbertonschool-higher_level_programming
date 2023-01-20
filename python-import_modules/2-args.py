54f90f008bbdaf016e321c01012126a1c343403a
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-20 18:56:51 +0000

    fix pycodestyle in file 2 in python-import_modules

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,13 @@ if __name__ == "__main__":
     from sys import argv
 
     argc = len(argv) - 1  # count the shell arguments, but not this script
-    print(f"{argc} argument{'' if argc == 1 else 's'}{'.' if not argc else ':'}")
+    print(argc, end=" ")
+    print(f"argument{'' if argc == 1 else 's'}{'.' if not argc else ':'}")
+    # possible outputs:
+    # 0 arguments.
+    # 1 argument:
+    # 2 arguments:
+    # ...
 
     argc += 1  # list the argument indexes from index 1 to the last,
     # without this executable
