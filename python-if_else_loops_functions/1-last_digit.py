49596b774bdded78eb9ac604ab6986741a0caef9
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-20 17:58:53 +0000

    made code much shorter and removed sign from 0

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,13 @@
 import random
 number = random.randint(-10000, 10000)
 
-last_digit = str(number)[-1]
-print(f"Last digit of {number} is {last_digit} and is", end=" -" if number < 0 else " ")
+last_digit = abs(number) % 10
+if number < 0:
+    last_digit *= -1
+# keep sign, unless it's 0 (so, -123 -> -3, -120 -> 0
+# and 123 -> 3)
+
+print(f"Last digit of {number} is {last_digit} and is", end=" ")
 
 if last_digit > 5:
     print("greater than 5")
@@ -11,4 +16,4 @@ elif last_digit == 0:
     print("0")
 else:
     print("less than 6 and not 0")
-
+# yes, we want to print "Last digit of 0 is 0 and is 0"
