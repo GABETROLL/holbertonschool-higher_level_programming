d165138ff61f00c41a9280ea38098b0d8dedf610
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-20 18:53:00 +0000

    print correct argument count in file 2 in python-import_modules

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     from sys import argv
 
     argc = len(argv) - 1  # count the shell arguments, but not this script
-    print(f"{argc} argument{'' if argc == 2 else 's'}{'.' if not argc else ':'}")
+    print(f"{argc} argument{'' if argc == 1 else 's'}{'.' if not argc else ':'}")
 
     argc += 1  # list the argument indexes from index 1 to the last,
     # without this executable
