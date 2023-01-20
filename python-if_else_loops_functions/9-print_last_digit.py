bf9920e6e1d7945a3161fff3ade550e2d1b42b0a
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-20 18:01:48 +0000

    Add true last digit check

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    pass
+    # This time, we don't care about the sign ;)
+    # (but Python does)
+    last_digit = abs(number) % 10
+    print(last_digit, end="")
+    return last_digit
