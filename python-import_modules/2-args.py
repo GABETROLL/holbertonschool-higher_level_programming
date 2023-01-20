b3e2cb7d3fd9723f03fdeb121a3d3393c8713f08
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-20 18:50:52 +0000

    add file 2: get shell arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    argc = len(argv) - 1  # count the shell arguments, but not this script
+    print(f"{argc} argument{'' if argc == 2 else 's'}{'.' if not argc else ':'}")
+
+    argc += 1  # list the argument indexes from index 1 to the last,
+    # without this executable
+    for index in range(1, argc):
+        argument = argv[index]
+        print(f"{index}: {argument}")
--- python-import_modules/README.md
+++ python-import_modules/README.md
@@ -2,3 +2,4 @@
 ## Directory:
 0. how to import a function (or any object or metaclass) from a file named _add\_0.py_
 1. how to import multiple functions (or any object or metaclass) from a file named _calculation\0.py_
+2. how to get and count the terminal arguments fed into a Python file
