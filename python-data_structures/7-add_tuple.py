4271bd5b32142671c206050b67876f2b03493760
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-23 17:25:14 +0000

    fixed immutable assignments and None edge case in exercise 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -8,13 +8,21 @@ def add_tuple(tuple_a=(), tuple_b=()):
     add_tuple((0), (1)) -> (1, 0)
     add_tuple(2, 3, 4, 5), (6, 7)) -> (2, 10)
     add_tuple() -> (0, 0)
+
+    it only adds the first two items of each
+    tuple, with their corresponding indexes,
+
+    it consideres the result (0, 0)
+    by default, in case a tuple is empty at
+    that index or is None
     """
-    result = (0, 0)
+    result = [0, 0]
 
     for index in range(2):
-        if index < len(tuple_a):
+        # new way to check if something is None! :)
+        if tuple_a and index < len(tuple_a):
             result[index] += tuple_a[index]
-        if index < len(tuple_b):
+        if tuple_b and index < len(tuple_b):
             result[index] += tuple_b[index]
 
-    return result
+    return tuple(result)
