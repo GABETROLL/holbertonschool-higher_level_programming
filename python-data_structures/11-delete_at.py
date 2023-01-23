256b35c58d9ffb813af118280e6ccf38ec08b8bc
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-23 18:47:44 +0000

    delete IN-PLACE in exercise 11 in python data structures

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -3,22 +3,25 @@
 
 def delete_at(my_list=[], idx=0):
     """
-    Copies the list 'my_list' then
-    deletes the item at index 'idx' in
+    Deletes the item at index 'idx' in
     list 'my_list', and slides all the remaining
-    elements into its place (like a soda fridge queue)
+    elements into its place
+    (like a soda fridge queue)
+
+    THEN RETURNS the (same) my_list
+    (due to the fact that by "passing" and
+    "returning" data structures, or any
+    'object' in Python, we're "passing"
+    the pointer to that object around in memory
+    (probably?)
 
     The challenge is to do this without using
     'list.pop'
 
     If 'my_list' is None, or idx is outside of the
-    list's range or negative, this function just returns
-    the original list.
+    list's range or negative, this function just skips
+    (read above) to returning the original list.
     """
-    if my_list is None or not (0 <= idx < len(my_list)):
-        return my_list
-
-    new_list = list(my_list)
-    del new_list[idx]
-
-    return new_list
+    if my_list and 0 <= idx < len(my_list):
+        del new_list[idx]
+    return my_list
