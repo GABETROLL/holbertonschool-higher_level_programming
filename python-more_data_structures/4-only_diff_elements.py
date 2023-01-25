ae1a0f47fa200dffd725a310e8d6f21a47c8644f
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-25 14:38:22 +0000

    Correct other difference missing in exercise 4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -7,4 +7,9 @@ def only_diff_elements(set_1, set_2):
     only show up in one of the two set
     parameters
     """
-    return set_1.difference(set_2)
+    # the set of the items that are only in set_1
+    # and the set of the items that are only in set_2
+    only_in_set_1 = set_1.difference(set_2)
+    only_in_set_2 = set_2.difference(set_1)
+
+    return only_in_set_1.union(only_in_set_2)
