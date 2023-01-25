a1e9d1364624513430054e9d700737870df72f62
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-25 17:09:03 +0000

    remove docstring from funciton in exercise 11

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
+# returns new list that's like 'my_list'
+# but has all its items multiplied by 'number'
 
+# The challenge here is to use 'map'
+# and to write the function in only 3 lines
 def multiply_list_map(my_list=[], number=0):
-    """
-    returns new list that's like 'my_list',
-    but has all its items multiplied by 'number'
-
-    The challenge here is to use 'map'
-    """
     if my_list is None:
         return
     return map(my_list, lambda x: x * 2)
