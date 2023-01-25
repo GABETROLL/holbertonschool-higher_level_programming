536f52323227449f6ccc1c8b23c8d9193b90660b
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-25 15:16:14 +0000

    return the dict in exercise 7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -4,10 +4,12 @@
 def update_dictionary(a_dictionary, key, value):
     """
     Sets key 'key' in 'a_dictionary' to be paired
-    with the value 'value'.
+    with the value 'value' (in-place)
+    then returns the 'a_dictionary'
 
     If the key is already present in the dict,
     it value gets replaced by 'value'.
     This is just how dicts work
     """
     a_dictionary[key] = value
+    return a_dictionary
