f0feb50ff841500eb33329cb5daf2ad7caedc77a
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-25 17:10:08 +0000

    make file exactly 3 lines

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
-
-# returns new list that's like 'my_list'
-# but has all its items multiplied by 'number'
-
-# The challenge here is to use 'map'
-# and to write the function in only 3 lines
 def multiply_list_map(my_list=[], number=0):
-    if my_list is None:
-        return
     return map(my_list, lambda x: x * 2)
