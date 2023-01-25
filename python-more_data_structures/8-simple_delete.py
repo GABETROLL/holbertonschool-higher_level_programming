8ae0432eec145686936b12eba8b8c258247b9745
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-25 15:35:03 +0000

    return dict in exercise 8

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -4,8 +4,10 @@
 def simple_delete(a_dictionary, key=""):
     """
     Deletes a key 'key' from the dictionary
-    'a_dictionary' if the key is present.
+    'a_dictionary' IN PLACE if the key is present,
+    then returns 'a_dictionary'
     """
     # None prevents the pop method raising a
     # KeyError when the key isn't in the dict
     a_dictionary.pop("key", None)
+    return a_dictionary
