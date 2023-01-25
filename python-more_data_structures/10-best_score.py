87ef8c65ae930b5f5ee06eef94fa15aff5345c9b
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-25 16:59:08 +0000

    add None ans empty edge cases

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -9,5 +9,8 @@ def best_score(a_dictionary):
     with the one adjacent to them (except
     the first and last)
     """
+    # 'max' doesn't allow empty iterables or None
+    if not a_dictionary:
+        return
     # dictionaries loop over their KEYS when iterated
     return max(a_dictionary, key=lambda k: a_dictionary[k])
