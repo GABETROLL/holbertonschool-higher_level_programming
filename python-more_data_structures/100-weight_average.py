54655c60c4ef6d2e7de44321efe81351bad87a96
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-26 14:52:25 +0000

    Add empty edge case to exercise 100

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -12,7 +12,12 @@ def weight_average(my_list=[]):
     if my_list == [(1, 2), (2, 1), (3, 10), (4, 2)]
     return (1 * 2) + (2 * 1) + (3 * 10) + (4 * 2))
         / (2 + 1 + 10 + 2)
+
+    If 'my_list' is empty, this function just
+    returns 0.
     """
+    if not my_list:
+        return 0
     a = sum(item[0] * item[1] for item in my_list)
     b = sum(item[1] for item in my_list)
     return a / b
