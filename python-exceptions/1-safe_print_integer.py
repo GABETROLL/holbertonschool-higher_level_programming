8df0204950e06d98333259d9c7ce2de4f3f5b094
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-27 14:39:07 +0000

    Add missing parenthesis and 'as error' in exercise 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -10,7 +10,17 @@ def safe_print_integer(value):
     """
     try:
         print("{:d}".format(value))
-    except TypeError, ValueError:
+    except (TypeError, ValueError) as error:
+        # when handling multiple errors in one
+        # code block, Python requires us to
+        # separate them by commas, inside
+        # parenthesis and to use the 'as'
+        # keyword to use the exception object.
+
+        # I _THINK_ it's because Python wants
+        # to make sure that you know EXACTLY
+        # which error was.
+
         # 'format' raises TypeError for NoneType,
         # tuple and list types, but ValueError
         # when given a string
