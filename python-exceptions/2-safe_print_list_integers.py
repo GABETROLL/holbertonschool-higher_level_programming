98d80fcabea81e25f836534a6435b474c6a2dac9
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-27 15:45:13 +0000

    made skipped elements not be counted in loop in exercise 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -14,7 +14,7 @@ def safe_print_list_integers(my_list=[], x=0):
     """
     integers_printed = 0
 
-    for index in range(x):
+    while integers_printed < x:
         try:
             print("{:d}".format(my_list[index]), end="")
         except IndexError:
