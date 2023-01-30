67240c1342c06e05b516533cf0cc97660449d9b7
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-30 16:52:15 +0000

    Actually set the value in exercise 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -52,6 +52,7 @@ class Square:
         if type(value) != tuple or len(value) != 2 \
                 or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
 
     def area(self):
         """
