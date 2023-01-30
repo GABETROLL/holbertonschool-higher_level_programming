c7eee46ebcd5fe1698f6b58b26c569eae4d72305
GABETROLL <gabriel7icplhappy@gmail.com>
2023-01-30 16:35:38 +0000

    Add exercise 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+How to customize a class' string representation
+(and therefore how it's printed)
+"""
+
+
+class Square:
+    """
+    'Square' class that has all of its
+    (theoretical) sides' lenghts as a 'size'
+    private attribute with its getters and
+    setters.
+
+    'size' MUST be an integer and MUST NOT be
+    negative.
+
+    Its string representation is a square of '#'
+    characters with width and height of 'self.size',
+    which is found in 'my_print'.
+    """
+    def __init__(self, size=0):
+        self.__size = None
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        returns the area of the square
+        (aka: 'self')
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Prints 'self' as a square of "#"
+        with sides of length 'self.size'.
+        """
+        print(("#" * self.size for _ in self.size), sep="\n")
--- python-classes/README.md
+++ python-classes/README.md
@@ -5,3 +5,4 @@
 2. How to create a class with a private attribute and an optional \_\_init\_\_ parameter
 3. How to create a class with a public method, that uses a private attribute
 4. How to create a getter and setter for a private instance attribute of a class
+5. How to create a method that prints the object
