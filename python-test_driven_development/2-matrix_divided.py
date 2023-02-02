789c2f4e557cad806333490880b21c76af078a44
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-02 15:03:27 -0400

    Add exercise 2

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Second (?) exercise to learn doctest
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Returns a new 'matrix' matrix with all its
+    elements divided by 'div', then rounded to 2
+    decimal places.
+
+    If 'matrix' is not a list of integers or floats,
+    TypeError is raised.
+    If matrix's rows are not all the same length,
+    TypeError is raised.
+    If 'div' isn't an 'int' or a 'float',
+    TypeError is raised.
+    If 'div' is '0',
+    ZeroDivisionError is raised.
+    """
+    if type(matrix) != list:
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not (type(div) in (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    result = []
+
+    for row in matrix:
+        if type(row) != list:
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+        if result and len(row) != len(result[-1]):
+            raise TypeError("Each row of the matrix must have the same size")
+
+        new_row = []
+
+        for x in row:
+            if not (type(x) in (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            new_row.append(round(x / div), 2)
+        result.append(new_row)
+
+    return result
