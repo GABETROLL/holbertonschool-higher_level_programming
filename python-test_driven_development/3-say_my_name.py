7c491dc96a41f8c64c1771889e24be7273674be9
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-02 15:29:32 -0400

    Add exersice 3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Exercise 3: add names with 'say_my_name'
+and check its edge cases with doctest
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Makes the person with the first name
+    'first_name' and the last name 'last_name'
+    introduce themselves like this:
+    
+    "My name is <first_name> <last_name>",
+    followed by a new line.
+
+    Unless 'first_name' or 'last_name' isn't
+    a string.
+    """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
