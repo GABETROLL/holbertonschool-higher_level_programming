2b3266703ac3d03419902d1406d6f059cc0e787a
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-03 11:19:48 -0400

    Add exercise 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+Exercise 2: Copy 'Rectangle' class from
+exercise 1 and add 'area' and 'perimeter'
+methods (that return the geometrical
+definitions of these)
+"""
+
+
+class Rectangle:
+    """
+    The width and height are properties with
+    getters and setters.
+
+    To set an attribute, it must be a positive
+    integer.
+    """
+    def __init__(self, width=0, height=0):
+        self.__width = None
+        self.__height = None
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+    
+    def area(self):
+        """
+        Returns this recangle's area according
+        to euclidian geometry, based in
+        'self.__width' and 'self.__height'.
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Returns this recangle's perimeter
+        according to euclidian geometry, based
+        in 'self.__width' and 'self.__height'.
+        """
+        return self.__width * 2 + self.__height * 2
--- python-more_classes/README.md
+++ python-more_classes/README.md
@@ -2,3 +2,4 @@
 ## Exercises by Holberton:
 0. Making an empty class
 1. Making a class with private attributes and their getters and setters
+2. Making an instance method in a class that uses its private attributes
