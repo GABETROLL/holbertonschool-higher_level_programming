9cb5b7b467a9aed96278d45d70d8967da78dceed
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-03 11:13:39 -0400

    Initialize properties in e1 in __init__ too

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -15,8 +15,11 @@ class Rectangle:
     integer.
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.__width = None
+        self.__height = None
+
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
