caeed2e303fa400a38fa720e6f5166acee880832
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-08 14:36:53 -0400

    Change grammar of the 2nd exercise's documentation

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -2,7 +2,7 @@
 """
 How to make a function that returns
 True if an object is an instance of
-an EXACT class (and not the class' children),
+an EXACT class, and not the class' children;
 otherwise, False.
 """
 
@@ -10,7 +10,7 @@ otherwise, False.
 def is_same_class(obj, a_class):
     """
     Returns True if 'obj' is an instance
-    of the EXACT class 'a_class' (AND NOT OF
-    THE CLASS' CHILDREN), otherwise, False.
+    of the EXACT class 'a_class', AND NOT OF
+    THE CLASS' CHILDREN; otherwise, False.
     """
     return type(obj) == a_class
