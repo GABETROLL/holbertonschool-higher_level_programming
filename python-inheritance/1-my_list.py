3b865e16d0cd95a2e18004edbd97c3b2fefa509e
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-08 13:38:57 -0400

    add doc strings to exercise 1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,5 +6,15 @@ the built-in class 'list'.
 
 
 class MyList(list):
+    """
+    Child class of the Python built-in
+    'list' class, with a method to print
+    its sorted version.
+    """
     def print_sorted(self):
+        """
+        Prints the sorted version of 'self',
+        by creating a new 'list' of 'self'
+        using 'sorted', then printing that.
+        """
         print(sorted(self))
