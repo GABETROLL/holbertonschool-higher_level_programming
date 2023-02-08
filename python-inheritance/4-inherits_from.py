55a9d88e6e1f26e951908c67269ea5d0577fff10
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-08 14:49:14 -0400

    Add class itself beinf an exception to exercise 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,10 +4,11 @@ How to know if the class of an instance
 inherited from another class
 """
 
+
 def inherits_from(obj, a_class):
     """
     Returns True if 'obj' is the instance
     of a class that inherits from 'a_class',
     False otherwise.
     """
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) != a_class
