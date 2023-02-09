0223e4a5a459363b787f16a7b753c0811c1c43d1
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-09 19:30:49 -0400

    exclude child classes of 'int' in exercise 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -20,7 +20,7 @@ class BaseGeometry:
         Throws an error if 'value' isn't an integer.
         Throws an error if 'value' is less or equal to 0.
         """
-        if not isinstance(value, int):
+        if type(value) != int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
