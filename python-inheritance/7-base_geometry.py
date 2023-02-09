9914b066f468b43a0b5267982761e7efda334dbb
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-09 19:31:23 -0400

    fix pycodestyle of exericise 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,7 +13,7 @@ class BaseGeometry:
     """
     def area(self):
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """
         Assumes that 'name' is always a string.
@@ -24,4 +24,3 @@ class BaseGeometry:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-        
