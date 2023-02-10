e4f593f5514496ef60aeef2501e9ea74ed48761a
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-09 20:23:18 -0400

    add exerise 101

---
+++ python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Exercise 101: make a function that adds
+an attribute to an object if possible
+"""
+
+
+def add_attribute(object, name, value):
+    """
+    Adds attribute named 'name' with
+    the value 'value' to the object
+    'object'.
+    """
+    object.__setattr__(name, value)
