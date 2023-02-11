92409ee99401622d49b9f30c26755ca4341b996d
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-11 16:21:31 -0400

    remove '.py' extensions from '__import__' calls in exercise 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,7 +5,7 @@ arguments to a Python list, and then saves the
 list to a JSON file called "add_item.json",
 in its JSON form.
 """
-save_to_json_file = __import__("5-save_to_json_file.py").save_to_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 if __name__ == "__main__":
     from sys import argv
