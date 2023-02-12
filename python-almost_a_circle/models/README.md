979c8a7f69a39dfa6363ce919df3c3e776132322
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-12 13:34:01 -0400

    add 'almost a circle' project

---
+++ python-almost_a_circle/models/README.md
@@ -0,0 +1,2 @@
+# 'Models' folder with a base class called 'Base'
+(no, you should never name a class 'Base', that's very vague...)
---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Exercise 0: Make a class called 'Base'.
+"""
+
+
+class Base:
+    """
+    creates a new instance with its individual
+    id, that represents how much instances of it
+    have been made before it, and itself.
+
+    '__nb_objects' counts the amount of instances
+    made since the start of the script.
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        self.id = id
