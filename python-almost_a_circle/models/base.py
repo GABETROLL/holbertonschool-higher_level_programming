32d46248fc34de548559f25490e5568066d84956
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-12 13:40:12 -0400

    add disclaimer in exercise 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -6,6 +6,10 @@ Exercise 0: Make a class called 'Base'.
 
 class Base:
     """
+    ('Base' is a very vague name, in my opinion,
+    you should consider a better name for your
+    classes)
+
     creates a new instance with its individual
     id, that represents how much instances of it
     have been made before it, and itself.
