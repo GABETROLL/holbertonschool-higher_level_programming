cacc3ea09e3219b360186a9f9a08395af2852141
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-12 20:11:58 -0400

    add documentation in README for square class

--- python-almost_a_circle/models/README.md
+++ python-almost_a_circle/models/README.md
@@ -1,1 +1,5 @@
-# A 'Rectangle' class that inherits from a 'Base' class
+# A 'Base', 'Rectangle' and 'Square' class
+## Directory
+_base.py_: a base class with an id for keeping track of instances, called 'Base'
+_rectangle.py_: a rectangle class that inherits from 'Base' with a string representation and size/position properties, called 'Rectangle'
+_square.py_: a square class that inherits from 'Rectangle', and uses the 'width' and 'height attributes from 'Rectangle' as one property called 'size'. This class is called 'Square'.
--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,10 +9,27 @@ class Square(Rectangle):
     """
     Functions the same way as 'Rectangle',
     but the width and height correspond the
-    'size' parameter.
+    'size' parameter and property.
     """
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """
+        the length of each side of 'self'
+        """
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value <= 0:
+            raise ValueError("size must be >= 0")
+
+        self.__width = value
+        self.__height = value
 
     def __str__(self):
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
