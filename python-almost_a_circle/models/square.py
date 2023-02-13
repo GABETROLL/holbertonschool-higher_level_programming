5dc06eeb0c6c0833d0f2c5c1fc3f59ca703d5957
GABETROLL <gabriel7icplhappy@gmail.com>
2023-02-12 20:17:54 -0400

    make error message in 'size' property be the same as 'width'

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -23,11 +23,6 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
-            raise TypeError("size must be an integer")
-        if value <= 0:
-            raise ValueError("size must be >= 0")
-
         self.width = value
         self.height = value
 
