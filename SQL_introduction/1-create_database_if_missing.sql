cb32b97e8c5f2b0cf1815fec4d09da218c5eb79b
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-09 15:13:58 -0400

    remove brackets form 1

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Exercise 1: create a database named "hbtn_0c_0".
 -- If the database already exists, an error shouldn't occur.
-CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
