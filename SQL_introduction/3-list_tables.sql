52d8d6fcf62a5cd515bf1fcf73e1bb25cdb57fef
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-09 16:13:19 -0400

    update 3's doc and add 5

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,7 +1,4 @@
 -- Exercise 3: show all the tables in a specific server.
--- The server is chosen in the MySql Shell's first argument.
+-- The server is chosen within the MySql Shell's arguments.
 
--- (the "&1" is the first argument
--- (the word after the "source 3-list_tables.sql")
--- )
 SHOW TABLES;
---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,6 @@
+-- Exercise 5: Print the full description of a
+-- table named "first_table" from the database
+-- "hbtn_0c_0".
+-- The database name will be passed in MySql's Shell arguments
+-- "DESCRIBE" or "EXPLAIN" are not allowed.
+EXEC sp_help tablename;
