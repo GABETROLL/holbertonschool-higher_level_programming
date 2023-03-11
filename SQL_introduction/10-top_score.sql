c0f0ca4ebe906ec7867e56e6e4e4d8e83cbbab15
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-11 13:40:17 -0400

    add specific columns

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,3 @@
 -- Exercise 10: list all records of the table 'second_table'.
 -- Order by highest-to-lowest scores.
-SELECT * FROM second_table ORDER BY score DESC;
+SELECT (name, score) FROM second_table ORDER BY score DESC;
