9b7072801686271b9f22c4676f06bbddab7e1864
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-11 12:39:24 -0400

    Add exercise 8

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,5 @@
+-- Exercise 8: Count all the rows that contain
+-- the value 89 in the 'id' column in the
+-- 'first_table' table.
+-- (and display the count in the console)
+SELECT COUNT(id=89) FROM first_table;
