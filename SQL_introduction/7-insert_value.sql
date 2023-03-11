5aaf880768c1de00c3190926a3442cab8bed5ebc
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-11 12:12:32 -0400

    add exercise 7

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,9 @@
+-- Exercise 7: Add a new row to the "first_table" table
+-- with the values being: id=89 name="Best School"
+INSERT INTO first_table VALUES (89, "Best School");
+
+-- Note: I can also be done by explicitly listing the column names:
+
+-- INSERT INTO first_table (id, name) VALUES (89, "Best School");
+
+-- Although I didn't test that line, so I can't say for sure...
