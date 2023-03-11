b812e9daae7ddab41af50e4e70d37f36d7526ec2
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-11 19:17:26 -0400

    Add exercice 12:

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- Exercise 11: update the score of "Bob" to 10
+-- in the table 'second_table'.
+-- Using "Bob"s id value is not allowed,
+-- only the 'name' field.
+UPDATE second_table SET score = 10 WHERE name = "Bob";
