931f374dddb8fcc7411a2012abc931cd70cd2ab4
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-11 19:46:44 -0400

    Add exercise 14

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,4 @@
+-- Exercise 14: compute the score average of all records
+-- inside the table 'second_table'.
+-- The result should be in a column named 'average'.
+SELECT AVG(score) AS average FROM second_table;
