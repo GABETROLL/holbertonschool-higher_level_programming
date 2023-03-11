1a26ba0d1ffdc9d6db85b6c666734d866d2bd9e5
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-11 13:47:52 -0400

    fix syntax

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,4 @@
 -- Exercise 10: list all records of the table 'second_table'.
 -- Order by highest-to-lowest scores.
-SELECT (name, score) FROM second_table ORDER BY score DESC;
+-- Only show the score and name columns, in that order.
+SELECT score, name FROM second_table ORDER BY score DESC;
