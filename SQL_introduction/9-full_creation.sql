d92e1dde90f6b78ba6c86d605191b15e30237c11
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-11 12:57:44 -0400

    change number to pass exercise

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@
 --  1 |  "John"  |   10
 --  2 |  "Alex"  |    3
 --  3 |  "Bob"   |   14
---  4 | "George" |   18
+--  4 | "George" |   8
 
 CREATE TABLE IF NOT EXISTS second_table
 (
@@ -18,4 +18,4 @@ CREATE TABLE IF NOT EXISTS second_table
 INSERT INTO second_table VALUES (1, "John", 10);
 INSERT INTO second_table VALUES (2, "Alex", 3);
 INSERT INTO second_table VALUES (3, "Bob", 14);
-INSERT INTO second_table VALUES (4, "George", 18);
+INSERT INTO second_table VALUES (4, "George", 8);
