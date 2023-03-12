98006a971e5500c2f90b6c6916b8196bd4657bc0
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-12 19:51:45 -0400

    fix syntax in 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,4 @@
 -- Exercise 16: Display all records in the 'second_table',
 -- but only if they contain a 'name' value.
 -- Display sorted by score, descending.
-SELECT * FROM second_table ORDER BY score DESC
-    WHERE score IS NOT NULL;
+SELECT * FROM second_table WHERE score IS NOT NULL ORDER BY score DESC;
