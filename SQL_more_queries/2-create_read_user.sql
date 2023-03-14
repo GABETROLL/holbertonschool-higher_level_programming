cde80ec4c253bc378de1bfcef5c61ff1d6fd1bf3
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-14 19:55:19 -0400

    remove quotes from 2 and indent 5

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -7,8 +7,8 @@
 -- this script shouldn't fail.
 
 -- create the database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS "hbtn_0d_2";
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- grant SELECT privilege
-GRANT SELECT ON "hbtn_0d_2".* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -2,4 +2,5 @@
 -- The table's description should be:
 -- (id INT with the default value 1 and must be unique,
 -- name VARCHAR(256))
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1, VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id
+    (id INT DEFAULT 1, VARCHAR(256));
