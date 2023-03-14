2d20768e9cfbbcdab6a80deeaf4c394148f81ff9
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-13 21:28:40 -0400

    correct exercise 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- Exercise 16: Display all records in the 'second_table',
 -- but only if they contain a 'name' value.
 -- Display sorted by score, descending.
-SELECT (score, name) FROM second_table WHERE score IS NOT NULL ORDER BY score DESC;
+SELECT (score, name) FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
