44797bda7f01c47b1ee6fa7bfcea1b73ba4c25f0
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-14 13:44:00 -0400

    fix syntax

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -4,6 +4,7 @@
 -- shouldn't fail.
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 -- (creating user first, then granting it privileges)
-GRANT ALL PRIVILEGES ON * TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * TO 'user_0d_1'@'localhost'
+    WITH GRANT OPTION;
 -- (using "GRANT * ON *" will only grant 'USAGE',
 -- according to what I'm seeing in the Holberton checkers)
