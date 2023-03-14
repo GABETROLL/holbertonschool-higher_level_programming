af77702ad59e4bcbf34c06058976a5b5ca0d8d11
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-14 13:32:54 -0400

    Add exercise 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Exercise 1: Create a new user called 'user_0d_1'.
+-- with all privileges, 'user_0d_1_pwd'.
+-- If the user 'user_0d_1' already exists, this script
+-- shouldn't fail.
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
