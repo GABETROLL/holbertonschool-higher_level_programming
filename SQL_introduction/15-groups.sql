e46da103114cfebad76298f3d6bc221bad2c432f
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-13 21:23:12 -0400

    add help and fix 15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,6 +1,26 @@
 -- Exercise 15: Display each INDIVIDUAL/distinct value
 -- in the 'second_table' table, and how many times
 -- it's trully present in the table.
-
+  
 -- (but only the score and number)
-SELECT DISTINCT score, COUNT(DISTINCT score) FROM second_table;
+SELECT score, COUNT(score) AS number FROM second_table
+    GROUP BY score
+    ORDER BY score DESC;
+
+-- (I THINK) the machine should look like this:
+-- (Yes, SQL is Turing complete)
+
+-- "FROM second_table":
+-- score: 5, 3, 6, 3, 7
+
+-- "SELECT score, COUNT(score) AS number":
+-- score:  5, 3, 6, 3, 7
+-- number: 1, 2, 1, 2, 1
+
+-- "GROUP BY score"
+-- score:  5, 3, 6, 7
+-- number: 1, 2, 1, 1
+
+-- "ORDER BY score DESC"
+-- score:  7, 6, 5, 3
+-- number: 1, 1, 1, 2
