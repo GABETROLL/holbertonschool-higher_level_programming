2376bdfccaf8373bc8e82e67507b00117d0eea86
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-14 19:59:15 -0400

    remove more quotes in 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -11,4 +11,4 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create user if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- grant SELECT privilege
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
