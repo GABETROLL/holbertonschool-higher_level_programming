d4c52f46aef8fbf8723d8c2a32fa673602e4f0e5
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-14 14:28:26 -0400

    Add exercise 2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,14 @@
+-- Exercise 2: Create a database 'hbtn_0d_2'.
+-- Create the user 'user_0d_2' with password 'user_0d_2_pwd'.
+-- Grant user 'user_0d_2' SELECT privilege in
+-- the database 'hbtn_0d_2'.
+-- If the database 'hbtn_0d_2' already exists,
+-- (inclusive)or the user 'user_0d_2' doesn't exist,
+-- this script shouldn't fail.
+
+-- create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+-- create user if it doesn't exist
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- grant SELECT privilege
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
