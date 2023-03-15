49b5af19d6b56e1aa33e81a4a921dc9476efbcd6
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-14 20:37:00 -0400

    Add exercise 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -8,4 +8,12 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa; -- <--
 CREATE TABLE IF NOT EXISTS
-    states (id INT NOT NULL, name VARCHAR(256));
+    states (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256));
+-- (according to w3schools.com) "PRIMARY KEY" does the same thing as
+-- "NOT NULL UNIQUE", and "UNIQUE" means the value
+-- can't repeat other values in the same column.
+
+-- (Also, according to w3schools.com):
+-- "AUTO_INCREMENT" can be used to auto-generate
+-- a new, unique number whenever a record is added.
+-- (It starts at 1 and increments by 1 for each new record)
