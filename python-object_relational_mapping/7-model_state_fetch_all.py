3bb4f47a1e1439a7330a126df510c212a6ac4937
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-17 14:31:23 -0400

    Add exercise 7

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Usage: ./7-model_state_fetch_all.py
+    <MySQL username>
+    <MySQL password>
+    <database name>
+
+Selects everything from the table named 'states'
+in the database logged in as the user provided
+in the arguments.
+
+Definition:
+-----------------------------------
+states ( 
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+)
+-----------------------------------
+"""
+from sqlalchemy import select, create_engine
+from model_state import Base, State
+
+if __name__ == "__main__":
+    from sys import argv
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(*argv[1:]))
+    output = select(State)
