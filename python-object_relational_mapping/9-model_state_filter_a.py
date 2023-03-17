b1aeae8f8247f71aeaf353656c04016468bb1a72
GABETROLL <gabriel7icplhappy@gmail.com>
2023-03-17 16:08:54 -0400

    Add exercies 9

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Usage: ./8-model_state_fetch_first.py
+    <MySQL username>
+    <MySQL password>
+    <database name>
+
+Prints all the State objects from the table
+named 'states', that have their names start with
+the letter a, in the database logged in as the
+user provided in the arguments. And prints the
+output.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    from sys import argv
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(*argv[1:])
+        )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    output = session.query(State)\
+                    .filter(State.name.contains("a") or \
+                            State.name.contains("A"))
+    
+    for row in output:
+        print(f"{row.id}: {row.name}")
+    
+    session.close()
