0ca1b4ec2caeb9563589007ef3f643c71440bd54
GABETROLL <gabriel7icplhappy@gmail.com>
2023-04-18 16:10:42 -0400

    disallow undefined as well

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -7,12 +7,14 @@
 // and 'h' to 'height'.
 
 // If 'w' or 'h' are lower or equal to 0,
+// or undefined,
 // the constructor creates
 // an empty object.
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
+    if (w <= 0 || w === undefined
+      || h <= 0 || h === undefined) {
       return;
     }
 
