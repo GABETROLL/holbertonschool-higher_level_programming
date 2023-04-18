791cec909f138bb1be45e31ffbd7d588debe7f89
GABETROLL <gabriel7icplhappy@gmail.com>
2023-04-18 10:22:55 -0400

    Add exercise 5

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// Exercise 5:
+// Print the "first" shell argument
+// converted to an integer,
+// if it can be converted to an integer.
+
+const process = require('process');
+const argv = process.argv;
+const argInt = parseInt(argv[2]);
+
+if (argInt !== undefined) {
+    console.log(argInt);
+}
