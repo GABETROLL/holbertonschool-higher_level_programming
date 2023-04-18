87246b91b2ddb040fdac4c5797a10ecc9a03d349
GABETROLL <gabriel7icplhappy@gmail.com>
2023-04-18 15:51:32 -0400

    make indentation 2 spaces

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -2,15 +2,15 @@
 
 // Exercise 1: make an Rectangle class
 // with 'width' and 'height' fields,
-// and a constructor that takes 'w' and 'h'
-// and assigns them to 'widht' and 'height'
-// (in that order).
+// and a constructor that takes args 'w' and 'h'
+// and assigns 'w' to 'width'
+// and 'h' to 'height'.
 
 class Rectangle {
-    constructor (w, h) {
-        this.width = w;
-        this.height = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 
 module.exports = Rectangle;
