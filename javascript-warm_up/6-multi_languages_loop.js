bbdb40dbd7d1424b2fcf01ee7e59efbdeae1eeb3
GABETROLL <gabriel7icplhappy@gmail.com>
2023-04-18 10:46:57 -0400

    use predetermined array

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -3,10 +3,9 @@
 // Exercise 6: Print messages by iterating
 // through an array, and using "console.log"
 // ONLY ONCE.
-// (except the first 2 in the argv array)
 
-const argv = require('process').argv;
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (index = 2; index < argv.length; index++) {
-  console.log(argv[index]);
+for (message of messages) {
+  console.log(message);
 }
