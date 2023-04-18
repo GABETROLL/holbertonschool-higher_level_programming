1828a06cc273db6bf4479a293ea23da4d05d0eb7
GABETROLL <gabriel7icplhappy@gmail.com>
2023-04-18 16:33:39 -0400

    Add exrecise 4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,44 @@
+#!/usr/bin/node
+
+// Exercise 3: make an Rectangle class
+// with 'width' and 'height' fields,
+// and a constructor that takes args 'w' and 'h'
+// and assigns 'w' to 'width'
+// and 'h' to 'height'.
+
+// If 'w' or 'h' are lower or equal to 0,
+// or undefined,
+// the constructor creates
+// an empty object.
+
+// make it have a 'print' method to it
+// that prints the Rectangle instance(s)
+// with the character 'X'.
+
+// make it have a 'rotate' method that exchanges
+// the Rectangle instances' 'width' and 'height'
+// fields.
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || w === undefined ||
+      h <= 0 || h === undefined) {
+      return;
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let h = 0; h < this.height; h++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+}
+
+module.exports = Rectangle;
