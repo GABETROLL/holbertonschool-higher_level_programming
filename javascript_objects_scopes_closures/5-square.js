0f186cae2a9bd3603870f3342555206609e278c8
GABETROLL <gabriel7icplhappy@gmail.com>
2023-04-18 19:37:24 -0400

    remove require call just to test

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -4,9 +4,6 @@
 // from the Rectangle class in exercise 4
 // and initializes its sides to a constructor argument,
 // 'size', using 'super'.
-
-const Rectangle = require('4-rectangle.js');
-
 class Square extends Rectangle {
   constructor (size) {
     super(size, size);
