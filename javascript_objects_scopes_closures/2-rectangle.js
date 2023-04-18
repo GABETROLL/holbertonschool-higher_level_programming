640aeda640b823e23cb43ca3e362bac8a3a4e38b
GABETROLL <gabriel7icplhappy@gmail.com>
2023-04-18 16:11:52 -0400

    fixx semistandard

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -13,8 +13,8 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || w === undefined
-      || h <= 0 || h === undefined) {
+    if (w <= 0 || w === undefined ||
+      h <= 0 || h === undefined) {
       return;
     }
 
