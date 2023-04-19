292a9880af75faeb43307db464941616d5e79110
GABETROLL <gabriel7icplhappy@gmail.com>
2023-04-19 10:18:03 -0400

    Add exercise 9

---
+++ javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Exercise 9: Write a function
+// that prints the number of arguments
+// already printed by it
+// and the new argument value
+
+let times = 0;
+
+exports.logMe = function (item) {
+  console.log(times + ': ' + item);
+};
