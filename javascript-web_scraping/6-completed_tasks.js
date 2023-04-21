925d923e86d35c389e56449df8767edcf8af1f56
GABETROLL <gabriel7icplhappy@gmail.com>
2023-04-20 20:24:49 -0400

    Add exercise 6

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+/*
+Exercise 6: make a script that computes
+the number of tasks completed by user id
+from the first shell argument,
+assuming that it's this API:
+https://jsonplaceholder.typicode.com/todos
+*/
+
+const request = require('request');
+
+const argv = require('process').argv;
+const API = argv[2];
+
+request(API, function (error, response, body) {
+    if (error) {
+      console.log('error: ' + response);
+      return;
+    }
+
+    const tasks = JSON.parse(body);
+
+    let usersAndTasks = {};
+
+    for (task of tasks) {
+        if (task.completed) {
+            if (usersAndTasks[task.userId] === undefined) {
+                usersAndTasks[task.userId] = 0;
+            } else {
+                usersAndTasks[task.userId]++;
+            }
+        }
+    }
+
+    console.log(usersAndTasks);
+}
+);
+  
