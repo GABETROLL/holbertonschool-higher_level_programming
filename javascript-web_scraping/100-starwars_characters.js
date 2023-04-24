a662d12569392c75aa705d81f9afef10f61868f0
GABETROLL <gabriel7icplhappy@gmail.com>
2023-04-24 18:43:53 -0400

    correct 100 URL with movie ID and add exception to it when no id arg is provided

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -4,29 +4,34 @@
 Exercise 7: Make a script that prints
 all the characters of a Star Wars movie
 in this API:
-https://swapi-api.hbtn.io/(api/films)
+https://swapi-api.hbtn.io/(api/films/<movie id arg>)
+where 'movie id arg' is a shell argument
+of the movie id (a base-10 natural)
 */
 
 const request = require('request');
-const { fileURLToPath } = require('url');
+const URL = 'https://swapi-api.hbtn.io/api/films/';
 
-const URL = 'https://swapi-api.hbtn.io/api/films';
-const movie_id = require('process').argv[2];
+const movieId = require('process').argv[2];
 
-request(URL, function (error, response, body) {
-	if (error) {
+if (movieId === undefined) {
+  throw new Error('Must provide a movie id arg.');
+  // This exception
+  // always ends the program
+  // when no arg is provided.
+}
+
+request(URL + movieId, function (error, response, body) {
+  if (error) {
     console.log('error: ' + response);
     return;
   }
-  const films = JSON.parse(body).results;
-  const film_character_urls = films[movie_id].characters;
-  // wrong
+  const films = JSON.parse(body);
 
-  for (const charcter_url of film_character_urls) {
+  const filmCharacterUrls = films.characters;
 
-    console.log('character_url: ' + charcter_url);
-
-    request(charcter_url, function (error, response, body) {
+  for (const charcterUrl of filmCharacterUrls) {
+    request(charcterUrl, function (error, response, body) {
       if (error) {
         console.log('error: ' + response);
         return;
